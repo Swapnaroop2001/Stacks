3203b7aad76d2aa19b42853912be7c7710d4bc55
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-18 16:58:52 -0500

    Update

--- Stackimplementation.java
+++ Stackimplementation.java
@@ -1,23 +1,24 @@
-public class Stackimplementation  {
-    int [] arr;
-    int top=-1;
-    int capacity; 
-    Stackimplementation(int size){
-        arr=new int[size];
-        capacity=size;
-        top=-1;
+public class Stackimplementation {
+    int[] arr;
+    int top = -1;
+    int capacity;
+
+    Stackimplementation(int size) {
+        arr = new int[size];
+        capacity = size;
+        top = -1;
     }
 
     public boolean isFull() {
-        return top==capacity-1;
+        return top == capacity - 1;
     }
 
     public void push(int x) {
         if (isFull()) {
             System.out.println("stack is full");
         } else {
-            top+=1;
-            arr[top]=x;
+            top += 1;
+            arr[top] = x;
         }
     }
 
@@ -27,16 +28,13 @@ public class Stackimplementation  {
         }
     }
 
-public static void main(String[] args) {  
-    Stackimplementation stk=new Stackimplementation(5);
-    stk.push(10);
-    stk.push(20);
-    stk.push(30);
-    stk.push(4);
-    stk.print();
+    public static void main(String[] args) {
+        Stackimplementation stk = new Stackimplementation(5);
+        stk.push(10);
+        stk.push(20);
+        stk.push(30);
+        stk.push(4);
+        stk.print();
 
-
-    
-
- }  
-}  
+    }
+}
