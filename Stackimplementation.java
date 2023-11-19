6288f33dea60463285107a3193b0c3e82e161695
Swapnaroop <swapnaroop2001@gmail.com>
2023-11-18 22:59:21 -0500

    Min stack

--- Stackimplementation.java
+++ Stackimplementation.java
@@ -18,7 +18,7 @@ public class Stackimplementation {
     }
 
     public void push(int x) {
-        if (isFull()) {
+        if (top == capacity - 1) {
             System.out.println("stack is full");
             return;
         } else {
@@ -28,20 +28,18 @@ public class Stackimplementation {
     }
 
     public void pop() {
-        if (top==-1) {
+        if (top == -1) {
             System.out.println("Stack is empty");
         } else {
-            System.out.println("Poped element :"+" "+arr[top]);
-            int []k=new int[top--];
+            System.out.println("Poped element :" + " " + arr[top]);
+            int[] k = new int[top--];
             for (int i = 0; i < k.length; i++) {
-                k[i]=arr[i];
+                k[i] = arr[i];
             }
             System.out.println("Stack after pop operation: ");
-            
-                for (int i = 0; i <= top; i++) {
-                    System.out.println(k[i]);
-                }
-            
+            for (int i = 0; i <= top; i++) {
+                System.out.println(k[i]);
+            }
         }
     }
 
@@ -51,16 +49,47 @@ public class Stackimplementation {
         }
     }
 
+    public int getMin() {
+        Arrays.sort(arr);
+        return arr[1];
+
+    }
+
+// Valid parentheses
+    public boolean isValid(String s) {
+        Stack <Character> stk = new Stack<Character>() ;
+        char []j=s.toCharArray();
+        for (int i = 0; i < j.length; i++) {
+            if (j[i]=='(') {
+                stk.push(')');
+            }
+            else if (j[i]=='{') {
+                stk.push('}');
+            }
+            else if (j[i]=='[') {
+                stk.push(']');
+            }
+            else if (stk.empty()||stk.pop()!=j[i]) {
+                return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+
+
+
     public static void main(String[] args) {
-        Stackimplementation stk= new Stackimplementation(5);
-        stk.push(10);
-        stk.push(20);
-        stk.push(30);
-        stk.push(40);
-        stk.push(50);
-        stk.push(60);
+        Stackimplementation stk = new Stackimplementation(5);
+        stk.push(4);
+        stk.push(26);
+        stk.push(5);
+        stk.push(7);
         
-        stk.pop();
+     
+
+        int m=stk.getMin();
+        System.out.println(m);
         
+
     }
 }
